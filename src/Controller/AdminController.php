d2f6fd713cf89c84e97df8a9e835a6953a92a75b
ClowClow <c.servieres@simplon-charleville.fr>
2019-10-05 14:01:03 +0200

    ajout des différentes pages de la partie admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,4 +16,34 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    /**
+     * @Route("/admin/espaceMusique", name="espaceMusique")
+     */
+    public function espaceMusique()
+    {
+        return $this->render('admin/espaceMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceVideo", name="espaceVideo")
+     */
+    public function espaceVideo()
+    {
+        return $this->render('admin/espaceVideo.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceUser", name="espaceUser")
+     */
+    public function espaceUser()
+    {
+        return $this->render('admin/espaceUser.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
 }
