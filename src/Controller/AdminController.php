2a1a2a38b566e780ab591094a8c7f2688addb8a4
ClowClow <c.servieres@simplon-charleville.fr>
2019-10-07 09:37:55 +0200

    ajout des boutons du menu musique

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -22,7 +22,87 @@ class AdminController extends AbstractController
      */
     public function espaceMusique()
     {
-        return $this->render('admin/espaceMusique.html.twig', [
+        return $this->render('admin/musique/espaceMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/addMusique", name="addMusique")
+     */
+    public function addMusique()
+    {
+        return $this->render('admin/musique/addMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/listMusique", name="listMusique")
+     */
+    public function listMusique()
+    {
+        return $this->render('admin/musique/listMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/modifyMusique", name="modifyMusique")
+     */
+    public function modifyMusique()
+    {
+        return $this->render('admin/musique/modifyMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/removeMusique", name="removeMusique")
+     */
+    public function removeMusique()
+    {
+        return $this->render('admin/musique/removeMusique.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/addAlbum", name="addAlbum")
+     */
+    public function addAlbum()
+    {
+        return $this->render('admin/musique/addAlbum.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/listAlbum", name="listAlbum")
+     */
+    public function listAlbum()
+    {
+        return $this->render('admin/musique/listAlbum.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/modifyAlbum", name="modifyAlbum")
+     */
+    public function modifyAlbum()
+    {
+        return $this->render('admin/musique/modifyAlbum.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/espaceMusique/removeAlbum", name="removeAlbum")
+     */
+    public function removeAlbum()
+    {
+        return $this->render('admin/musique/removeAlbum.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
@@ -32,7 +112,7 @@ class AdminController extends AbstractController
      */
     public function espaceVideo()
     {
-        return $this->render('admin/espaceVideo.html.twig', [
+        return $this->render('admin/video/espaceVideo.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
@@ -42,7 +122,7 @@ class AdminController extends AbstractController
      */
     public function espaceUser()
     {
-        return $this->render('admin/espaceUser.html.twig', [
+        return $this->render('admin/user/espaceUser.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
